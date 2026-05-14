7ce17065dbbc66f64b36f6dbc9ef9ec4996375a6
Vyshnav <your-email@gmail.com>
2026-05-14 11:33:22 +0530

    fixed backend route

--- frontend/script.js
+++ frontend/script.js
@@ -15,8 +15,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch(
-            "https://portfolio-backend-c82r.onrender.com",
+        const response = await fetch("https://portfolio-backend-c82r.onrender.com/contact",
             {
                 method: "POST",
 
